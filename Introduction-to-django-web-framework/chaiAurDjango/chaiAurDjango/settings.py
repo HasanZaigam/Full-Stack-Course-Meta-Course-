d2e9a17e400e69e31efa79c7a272f7033b9a6e74
HasanZaigam <syedhasanzaigam50890@gmail.com>
2025-12-14 22:35:10 +0530

    successfully installed the tailwind css

--- Introduction-to-django-web-framework/chaiAurDjango/chaiAurDjango/settings.py
+++ Introduction-to-django-web-framework/chaiAurDjango/chaiAurDjango/settings.py
@@ -38,7 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'chai',
+    'tailwind', 
+    'tailwind_django',
 ]
+
+TAILWIND_APP_NAME = 'tailwind_django'
+INTERNAL_IPS = ['127.0.0.1']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
---
+++ Introduction-to-django-web-framework/chaiAurDjango/tailwind_django/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Tailwind_djangoConfig(AppConfig):
+    name = 'tailwind_django'
