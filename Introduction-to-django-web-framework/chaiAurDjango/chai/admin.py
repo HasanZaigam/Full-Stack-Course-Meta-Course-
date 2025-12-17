9e2adfdd2bef8d71e2618aa1382d41c8936d7587
HasanZaigam <syedhasanzaigam50890@gmail.com>
2025-12-18 01:37:28 +0530

    starterd with the models

--- Introduction-to-django-web-framework/chaiAurDjango/chai/admin.py
+++ Introduction-to-django-web-framework/chaiAurDjango/chai/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import ChaiVarieties
 
 # Register your models here.
+admin.site.register(ChaiVarieties)
--- Introduction-to-django-web-framework/chaiAurDjango/chai/views.py
+++ Introduction-to-django-web-framework/chaiAurDjango/chai/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
+from .models import ChaiVarieties
 # Create your views here.
 def all_chai(request):
     return render(request,'chai/all_chai.html')
 
 def order(request):
-    return render(request, 'chai/order.html')
+    orders = ChaiVarieties.objects.all()
+    return render(request, 'chai/order.html', {'orders_Chais' : orders})
