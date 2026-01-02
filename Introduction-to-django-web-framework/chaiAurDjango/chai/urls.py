50c740d9a3b4e73a942e0ee7f9e21b760ce510b6
HasanZaigam <syedhasanzaigam50890@gmail.com>
2026-01-03 00:54:12 +0530

    updatation till model creation, cross page navigation and so on

--- Introduction-to-django-web-framework/chaiAurDjango/chai/urls.py
+++ Introduction-to-django-web-framework/chaiAurDjango/chai/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.all_chai, name = "all_chai"),
     path('order/', views.order, name = "chaiOrders"),
+    path('<int:chai_id>/', views.chai_des, name = "chai_description")
 ]
--- Introduction-to-django-web-framework/chaiAurDjango/chai/views.py
+++ Introduction-to-django-web-framework/chaiAurDjango/chai/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import ChaiVarieties
+from django.shortcuts import get_object_or_404
 # Create your views here.
 def all_chai(request):
     return render(request,'chai/all_chai.html')
@@ -7,3 +8,9 @@ def all_chai(request):
 def order(request):
     orders = ChaiVarieties.objects.all()
     return render(request, 'chai/order.html', {'orders_Chais' : orders})
+
+def chai_des(request, chai_id):
+    chai_desc = get_object_or_404(ChaiVarieties, pk = chai_id)
+    return render(request, 'chai/chai_description.html', {'chai_desc' : chai_desc})
+
+
